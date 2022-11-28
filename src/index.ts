e4ee041c85b644360c7b0f43925d48bf53683d19
NguyenHHKiet <1851050073kiet@ou.edu.vn>
2022-11-28 10:55:27 +0700

    list task

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,55 @@
-// import { v4 as uuidv4 } from 'uuid';
-// // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
-// console.log(uuidv4());
+import { v4 as uuidv4 } from 'uuid';
+
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
 const list = document.querySelector<HTMLDListElement>('#list');
 const form = document.getElementById('new-task-form') as HTMLFormElement | null;
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
 
+const addListItem = (task: Task) => {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked;
+    saveTasks();
+  });
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+};
+
+const saveTasks = () => {
+  localStorage.setItem('TASKS', JSON.stringify(tasks));
+};
+
+const loadTasks = (): Task[] => {
+  const taskJSON = localStorage.getItem('TASKS');
+  if (taskJSON == null) return [];
+  return JSON.parse(taskJSON);
+};
+
+const tasks: Task[] = loadTasks();
+tasks.forEach(addListItem);
+
 form?.addEventListener('click', (e) => {
   e.preventDefault();
 
-  if (input?.value == '' || input?.value == null) {
-    console.log('first');
-  }
+  if (input?.value == '' || input?.value == null) return;
+  const newTask: Task = {
+    id: uuidv4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+  tasks.push(newTask);
+  saveTasks();
+  addListItem(newTask);
+  input.value = '';
 });
